1a4f254abeb7fa19b9d3b4d33e195740142ccdb1
feelsgoodfrog <gudrb963@gmail.com>
2026-04-28 22:37:38 +0900

    feat: add `*ghghghkos-projects.vercel.app` cors

--- src/main/java/com/runmarket/api/config/SecurityConfig.java
+++ src/main/java/com/runmarket/api/config/SecurityConfig.java
@@ -55,7 +55,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.runmarket.cc"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://www.runmarket.cc",
+                "https://*ghghghkos-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
