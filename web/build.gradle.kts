35d614a97e2e502eedc1efef3d2218894a50ce6e
feelsgoodfrog <gudrb963@gmail.com>
2026-05-25 18:56:55 +0900

    feat: jwt moved infra module

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -14,8 +14,6 @@ jib {
     }
 }
 
-val jjwt = "0.13.0"
-
 dependencies {
     implementation(project(":application"))
 
@@ -23,10 +21,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    implementation("io.jsonwebtoken:jjwt-api:$jjwt")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwt")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwt")
 
     runtimeOnly(project(":infrastructure"))
     runtimeOnly(project(":event-bus"))
